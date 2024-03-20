5da8284f4e72bc0682cec29fdb3205fcb758683b
NoumanAnwar <164159073+NoumanAnwar@users.noreply.github.com>
2024-03-20 14:54:32 +0500

    My Hello World 1st Assignment

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+var food = "biryani";
+console.log(food);
+var num1 = 1;
+var num2 = 2;
+console.log(num1 + num2);
---
+++ main.ts
@@ -0,0 +1,6 @@
+console.log("hello world");
+let food = "biryani";
+console.log(food);
+let num1=1
+let num2=2
+console.log(num1+num2);
